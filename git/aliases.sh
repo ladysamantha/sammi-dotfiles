ffa82192dd66cd6cb3184e5fd0d382cd10827003
Samantha Enders <ladysamantha@protonmail.com>
2018-04-28 14:47:45 -0600

    Changed colors in `git lg`

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-GIT=$(command -v git)
-
-if [[ ! -x $GIT ]]; then
+command -v git >/dev/null 2>&1 || {
     echo "git is not installed or in your PATH"
     exit 1
-fi
+}
 
 git config --global alias.cp 'cherry-pick'
 git config --global alias.st 'status -sb'
@@ -14,7 +12,7 @@ git config --global alias.ci 'commit'
 git config --global alias.co 'checkout'
 git config --global alias.br 'branch'
 git config --global alias.dc 'diff --cached'
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit --date=relative --all"
+git config --global alias.lg "log --graph --pretty=format:'%C(magenta)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit --date=relative --all"
 git config --global alias.last 'log -1 --stat'
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.up 'push'
