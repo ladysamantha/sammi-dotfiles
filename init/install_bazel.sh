ad5e382cc315756b7b3866d4ada628f60efc0b71
Samantha Enders <ladysamantha@protonmail.com>
2018-05-06 15:39:04 -0600

    Remove excess logging

--- init/install_bazel.sh
+++ init/install_bazel.sh
@@ -26,7 +26,7 @@ pushd /tmp || (echo "Failed to cd to /tmp" && exit 255)
 curl -sSL "${BAZEL_URL}" > "${BAZEL_FILE}"
 curl -sSL "${BAZEL_SHA_URL}" > "${BAZEL_SHA_FILE}"
 
-cat "$BAZEL_FILE"
+ls -lHa
 cat "$BAZEL_SHA_FILE"
 
 sha256sum -c "${BAZEL_URL}" >/dev/null 2>&1 || {
