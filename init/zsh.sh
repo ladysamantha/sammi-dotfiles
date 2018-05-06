846c9d4c0dffb8805f25b550a5cc205444d02d1b
Samantha Enders <ladysamantha@protonmail.com>
2018-05-06 12:44:32 -0600

    Add check for existing zshrc file

--- init/zsh.sh
+++ init/zsh.sh
@@ -3,5 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$( (cd "$DIR/.." && pwd) || exit 1)"
 
+if [[ -f "$HOME/.zshrc" ]]; then
+    mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+fi
+
 ln -s "$ROOT_DIR/zsh/zshrc.zsh" "$HOME/.zshrc"
 
